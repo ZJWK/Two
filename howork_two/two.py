b1885145728a357bc65b19f584a056cedaa26b18
ZJWK <591564847@qq.com>
2019-06-27 00:10:31 +0800

    Initial commit

---
+++ howork_two/two.py
@@ -0,0 +1,17 @@
+# 一、变量命名
+# 变量名只能包含字母、数字和下划线。变量名可以字母或下划线打头，但不能以数字打头，例如，可将变量命名为message_1，但不能将其命名为1_message。
+# 　　 变量名不能包含空格，但可使用下划线来分隔其中的单词。例如，变量名greeting_message可行，但变量名greeting message会引发错误。
+# 　　 不要将Python关键字和函数名用作变量名，即不要使用Python保留用于特殊用途的单词，如print。
+
+# 　　 变量名应既简短又具有描述性。例如， name比n好， student_name比s_n好， name_length比length_of_persons_name好。
+# 　　 慎用小写字母l和大写字母O，因为它们可能被人错看成数字1和0。
+
+# 二、input函数：
+# input函数在python中是一个内建函数，其从标准输入中读入一个字符串，并自动忽略换行符。
+
+# 华氏温度＝摄氏温度x1.8+32.
+huishi=float(input('请输入华氏温度：'))
+# sheshi=float(input('请输入摄氏温度：'))
+sheshi=(huishi-32)/1.8
+print('华氏温度转换为摄氏温度为：',sheshi)
+
