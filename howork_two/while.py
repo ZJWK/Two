b6ae141923e048038618d1f2d734999629a80936
ZJWK <591564847@qq.com>
2019-07-17 00:19:28 +0800

    Fibonacci

---
+++ howork_two/while.py
@@ -0,0 +1,14 @@
+# 循环语句  while
+
+# 无限循环，while的设置条件不能为常量，否则会会无限循环
+'''
+CONDITION = True
+while CONDITION:
+     print('I am while')
+'''
+
+# for循环 主要用来遍历、循环、序列或者集合、字典
+a = ['apple', 'orange', 'banana', 'grape']
+for x in a:
+    pow(x)
+
--- howork_two/xunhuan.py
+++ howork_two/xunhuan.py
@@ -1,6 +1,6 @@
 # 印象笔记
 # https://app.yinxiang.com/shard/s20/nl/19065121/475b943c-afea-4f6d-83c6-fe9707f4ab76?title=Python%E4%B9%8B%20for%E5%BE%AA%E7%8E%AF
-
+# input()函数默认接收的是字符串
 account = 'python'
 password = '123456'
 print('please input account:')
@@ -11,3 +11,5 @@ if account == user_account and password == user_password:
     print('success')
 else:
     print('fail')
+
+
