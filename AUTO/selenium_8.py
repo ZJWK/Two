7864693cee704b2f0e3b7c1a33fa03e49300aeaa
ZJWK <591564847@qq.com>
2019-07-23 00:10:53 +0800

    selenium

---
+++ AUTO/selenium_8.py
@@ -0,0 +1,17 @@
+# selenium下载地址：https://pypi.org/project/selenium/
+# 在selenium安装文件下shift+右键，然后输入命令
+# python setup.py install安装
+# 判断selenium是否安装成功：(在执行该命令时记得切换到python的安装根目录下)pip list  或者在python环境下 import selenium,如果没有报错的话
+# 就代表成功安装了selenium
+# selenium验证安装：将浏览器驱动复制到Python安装路径
+# 谷歌浏览器驱动下载地址:https://chromedriver.storage.googleapis.com/index.html
+
+# from selenium import webdrive
+# drive = webdrive.Firefox()
+
+
+
+'''
+前端知识讲解：
+
+'''
