ea2b854bd42c79a33d70bb736d0e7b7a5a0d48a6
ZJWK <591564847@qq.com>
2019-07-31 22:18:04 +0800

    增加截图功能

--- AUTO/login_11_2.py
+++ AUTO/login_11_2.py
@@ -21,22 +21,28 @@ pwdP.clear()
 pwdP.send_keys(pwd)
 
 # 点击“记住登录密码”单选框
-auto_login = driver.find_element_by_id("auto-signin").click()
-
+try:
+    auto_login = driver.find_element_by_id("auto-signin").click()
 
 # 查找登录按钮并摁下
-btn = driver.find_element_by_css_selector("input.moco-btn").click()
+    btn = driver.find_element_by_css_selector("input.moco-btn").click()
+
 
 # 查找用户头像
-sleep(3)
-user_info = driver.find_element_by_css_selector("#header-avator > img:nth-child(1)")
-ActionChains(driver).move_to_element(user_info).perform()
-
+    sleep(3)
+    user_info = driver.find_element_by_css_selector("#header-avator > img:nth-child(1)")
+    ActionChains(driver).move_to_element(user_info).perform()
+except :
+    driver.get_screenshot_as_file("D:\\work\\AUTO_test\\Two\\截图\\登录失败.png")    #截图并保存在该路径下
 # 查找当前登录用户名
 name = driver.find_element_by_css_selector(".name")
 print(name.text)
+
 # 判断是否登录成功
 if name.text == "慕粉3793841":
-    print("login success")
+     print("login success")
+     driver.get_screenshot_as_file("D:\\work\\AUTO_test\\Two\\截图\\登录成功.png")
 else:
     print("login fail")
+
+
